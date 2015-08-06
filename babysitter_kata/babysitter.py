9c4aa575387907f058b41dd5c2144fea85a0df4c
Valentine Blacker <valentineblacker@gmail.com>
2015-08-05 21:18:03 -0400

    added test for bed time

--- babysitter_kata/babysitter.py
+++ babysitter_kata/babysitter.py
@@ -1,25 +1,32 @@
 
-import datetime
+from datetime import time as time
 
 class Babysit(object):
     
-    def __init__(self, starttime = datetime.time(00,00), bedtime = datetime.time(00,00), endtime = datetime.time(00,00)):
+    def __init__(self, starttime = time(00,00), bedtime = time(00,00), endtime = time(00,00)):
         self.start_time = starttime
         self.bed_time = bedtime
         self.end_time = endtime
         
     def check_start_time(self, starttime):
         #returns TRUE if start time is acceptable. Can start any time between 5:00 pm and 4:00 am
-        if starttime >= datetime.time(17,00) or starttime <= datetime.time(04,00):
+        if starttime >= time(17,00) or starttime <= time(04,00):
             return True
         else: return False
 
     def check_end_time(self, endtime):
         #returns TRUE if end time is acceptable. Must end at or before 4:00 am. May end as early as 5:00 pm
-        if endtime <= datetime.time(04,00) or endtime >= datetime.time(17,00):
+        if endtime <= time(04,00) or endtime >= time(17,00):
             return True
         else: return False
 
+    def check_bed_time(self, bedtime):
+        #returns TRUE if bedtime is acceptable. Must be between 5 pm and midnight
+        if bedtime <= time(23,59) and bedtime >= time(17,00):
+            return True
+        elif bedtime == time(00,00):
+            return True
+        else: return False
 
 if __name__ == '__main__':
     main = Babysit
--- babysitter_kata/test_babysitter.py
+++ babysitter_kata/test_babysitter.py
@@ -1,5 +1,5 @@
 import unittest
-import datetime
+from datetime import time as time
 from babysitter import Babysit
 
 class testBabysitter(unittest.TestCase):
@@ -13,18 +13,24 @@ class testBabysitter(unittest.TestCase):
 
     def testStartTime(self):
         #can't start too early
-        self.assertTrue(Babysit.check_start_time(self.babysitter, datetime.time(17,00)))
-        self.assertTrue(Babysit.check_start_time(self.babysitter, datetime.time(04,00)))
-        self.assertFalse (Babysit.check_start_time(self.babysitter, datetime.time(16,59)))
-        self.assertFalse (Babysit.check_start_time(self.babysitter, datetime.time(04,01)))
+        self.assertTrue(Babysit.check_start_time(self.babysitter, time(17,00)))
+        self.assertTrue(Babysit.check_start_time(self.babysitter, time(04,00)))        
+        self.assertFalse (Babysit.check_start_time(self.babysitter, time(16,59)))
+        self.assertFalse (Babysit.check_start_time(self.babysitter, time(04,01)))        
 
     def testEndTime(self):
         #can't work too late
-        self.assertTrue(Babysit.check_end_time(self.babysitter, datetime.time(04,00)))
-        self.assertTrue(Babysit.check_end_time(self.babysitter, datetime.time(17,00)))
-        self.assertFalse(Babysit.check_end_time(self.babysitter, datetime.time(04,01)))
-        self.assertFalse(Babysit.check_end_time(self.babysitter, datetime.time(16,59)))
+        self.assertTrue(Babysit.check_end_time(self.babysitter, time(04,00)))
+        self.assertTrue(Babysit.check_end_time(self.babysitter, time(17,00)))
+        self.assertFalse(Babysit.check_end_time(self.babysitter, time(04,01)))
+        self.assertFalse(Babysit.check_end_time(self.babysitter, time(16,59)))        
 
+    def testBedTime(self):
+        #kids need to be in bed before midnight, but after we've started sitting
+        self.assertTrue(Babysit.check_bed_time(self.babysitter, time(23,59)))
+        self.assertTrue(Babysit.check_bed_time(self.babysitter, time(17,00)))
+        self.assertFalse(Babysit.check_bed_time(self.babysitter, time(00,01)))
+        self.assertFalse(Babysit.check_bed_time(self.babysitter, time(04,59)))
     
         
 
