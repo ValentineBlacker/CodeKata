79bb645721177b11926a4b41ad0cbb09f05d12f2
Valentine Blacker <valentineblacker@gmail.com>
2015-08-06 22:42:00 -0400

    refactored for PEP8 compatibility

--- babysitter_kata/babysitter.py
+++ babysitter_kata/babysitter.py
@@ -1,33 +1,38 @@
 from datetime import time
 import sys
 
+
 class Babysit(object):
+
     """Calculate's a babysitter's pay. Pass 3 datetime objects to
     test_and_calculate() if using object from another program.
     Also runs handily in the terminal, and does error-checking
     on raw strings. """
-    
     def __init__(self):
         self.start_to_bed = 12
         self.bed_to_mid = 8
         self.mid_to_end = 16
-                
+
     def check_start_or_end_time(self, thetime):
-        #returns TRUE if start time is acceptable. Can start any time between 5:00 pm and 4:00 am       
-        if thetime >= time(17,00) or thetime <= time(04,00):
+        # returns TRUE if start time is acceptable. Can start any time between
+        # 5:00 pm and 4:00 am
+        if thetime >= time(17, 00) or thetime <= time(04, 00):
             return True
-        else: return False   
+        else:
+            return False
 
     def check_bed_time(self, bedtime):
-        #returns TRUE if bedtime is acceptable. Must be between 5 pm and midnight
-        if bedtime <= time(23,59) and bedtime >= time(17,00):
+        # returns TRUE if bedtime is acceptable.
+        # Must be between 5 pm and midnight
+        if bedtime <= time(23, 59) and bedtime >= time(17, 00):
             return True
-        elif bedtime == time(00,00):
+        elif bedtime == time(00, 00):
             return True
-        else: return False
+        else:
+            return False
 
     def check_times_for_rationality(self, starttime, bedtime, endtime):
-        #make sure endtime is after bedtime, and bedtime is after starttime
+        # make sure endtime is after bedtime, and bedtime is after starttime
         endtime_ok = False
         if endtime.hour < 5:
             endtime_ok = True
@@ -36,54 +41,56 @@ class Babysit(object):
         elif endtime.hour == bedtime.hour and endtime.minute >= bedtime.minute:
             endtime_ok = True
         bedtime_ok = False
-        
+
         if bedtime.hour > starttime.hour:
             bedtime_ok = True
-        elif bedtime.hour == starttime.hour and bedtime.minute >= starttime.minute:
+        elif bedtime.hour == starttime.hour\
+                and bedtime.minute >= starttime.minute:
             bedtime_ok = True
-            
-        if endtime_ok == True and bedtime_ok == True:
+
+        if endtime_ok is True and bedtime_ok is True:
             return True
-        else: return False            
+        else:
+            return False
 
     def pay_start_to_bed(self, starttime, bedtime):
-        #we deal only with hours here, since we get paid for a full hour at start
-        #even if we begin at the half hour, and if bedtime is in the middle of an hour
-        #we get paid the higher rate for that entire hour
-        #so we get paid at least 1 hour at the pre-bedtime rate
+        """we deal only with hours here, since we get paid for a full hour at
+        start even if we begin at the half hour, and if bedtime is in
+        the middle of an hour we get paid the higher rate for that entire hour
+        so we get paid at least 1 hour at the pre-bedtime rate"""
         bedtime_hour = int(bedtime.hour)
         if bedtime_hour == 0:
             bedtime_hour = 24
         if int(bedtime.minute) > 0:
-            bedtime_hour += 1        
-        number_of_hours = (bedtime_hour - int(starttime.hour))      
+            bedtime_hour += 1
+        number_of_hours = (bedtime_hour - int(starttime.hour))
         if number_of_hours == 0:
             number_of_hours = 1
         total = (number_of_hours * self.start_to_bed)
         return total
 
     def pay_bed_to_mid(self, bedtime, endtime):
-        #calculates to end time if end time is before midnight.
-        #otherwise calculates to midnight. Partial hours paid
-        #at higher pre-bedtime rate.
+        # calculates to end time if end time is before midnight.
+        # otherwise calculates to midnight. Partial hours paid
+        # at higher pre-bedtime rate.
         ending_hour = int(endtime.hour)
-        #if ending after midnight, calculate from midnight
+        # if ending after midnight, calculate from midnight
         if ending_hour < 17:
             ending_hour = 24
-        #otherwise round hour up
+        # otherwise round hour up
         elif int(endtime.minute) > 0:
             ending_hour += 1
         bedtime_hour = int(bedtime.hour)
         if int(bedtime.minute) > 0:
-            bedtime_hour += 1        
-        number_of_hours = ending_hour - bedtime_hour        
+            bedtime_hour += 1
+        number_of_hours = ending_hour - bedtime_hour
         total = (number_of_hours * self.bed_to_mid)
         return total
 
     def pay_mid_to_end(self, endtime):
-        #calculates pay from midnight to end time
-        #returns 0 if endtime is before midnight.
-        #pays full hour for any partials.
+        # calculates pay from midnight to end time
+        # returns 0 if endtime is before midnight.
+        # pays full hour for any partials.
         ending_hour = int(endtime.hour)
         if ending_hour > 4:
             return 0
@@ -93,95 +100,109 @@ class Babysit(object):
         return total
 
     def calculate_total(self, starttime, bedtime, endtime):
-        #calculates total pay
-        stb = self.pay_start_to_bed(starttime, bedtime)        
-        btm = self.pay_bed_to_mid(bedtime, endtime)        
-        mte = self.pay_mid_to_end(endtime)        
+        # calculates total pay
+        stb = self.pay_start_to_bed(starttime, bedtime)
+        btm = self.pay_bed_to_mid(bedtime, endtime)
+        mte = self.pay_mid_to_end(endtime)
         total = stb+btm+mte
         return total
 
     def test_and_calculate(self, starttime, bedtime, endtime):
-        #test input. returns NONE if times are not allowed, otherwise returns total
+        # test input. returns NONE if times are not allowed,
+        # otherwise returns total
         start_time_ok = self.check_start_or_end_time(starttime)
         end_time_ok = self.check_start_or_end_time(endtime)
         bed_time_ok = self.check_bed_time(bedtime)
-        times_rational = self.check_times_for_rationality(starttime, bedtime, endtime)
+        times_rational = self.check_times_for_rationality(
+            starttime, bedtime, endtime)
         for x in [start_time_ok, end_time_ok, bed_time_ok, times_rational]:
-            if x == False:
+            if x is False:
                 return None
-        else: total = self.calculate_total(starttime, bedtime, endtime)
+        else:
+            total = self.calculate_total(starttime, bedtime, endtime)
         return total
 
     def convert_raw_input(self, rawstring):
-        #converts raw input to date object.
-        #returns NONE if input cannot be converted.
-        if len(rawstring) >5 or len(rawstring) < 4:
+        # converts raw input to date object.
+        # returns NONE if input cannot be converted.
+        if len(rawstring) > 5 or len(rawstring) < 4:
             return None
-        if rawstring[:2].isdigit() and rawstring[3:].isdigit() and rawstring[2] == ':':       
+        if rawstring[:2].isdigit() and rawstring[3:].isdigit()\
+                and rawstring[2] == ':':
             hours = int(rawstring[:2])
             minutes = int(rawstring[3:])
             if hours < 24 and minutes < 60:
-                return time(hours,minutes)
-            else: return None
-        else: return None
+                return time(hours, minutes)
+            else:
+                return None
+        else:
+            return None
 
     def handle_start_input(self, starttime):
-        #checks start time input when called from terminal
+        # checks start time input when called from terminal
         starttime = self.convert_raw_input(starttime)
-        if starttime == None:
+        if starttime is None:
             print ('Please format time correctly. Program now exiting.\n')
-            return None        
-        else: return starttime
+            return None
+        else:
+            return starttime
 
     def handle_bed_input(self, bedtime):
-        #checks bed time input when called from terminal
+        # checks bed time input when called from terminal
         bedtime = self.convert_raw_input(bedtime)
-        if bedtime == None:
+        if bedtime is None:
             print ('Please format time correctly. Program now exiting.\n')
-            return None        
-        else: return bedtime
+            return None
+        else:
+            return bedtime
 
     def handle_end_input(self, endtime):
-        #checks end time input when called from terminal
+        # checks end time input when called from terminal
         endtime = self.convert_raw_input(endtime)
-        if endtime == None:
+        if endtime is None:
             print ('Please format time correctly. Program now exiting.\n')
-            return None        
-        else: return endtime
-                
+            return None
+        else:
+            return endtime
+
 if __name__ == '__main__':
     main = Babysit
     babysitter = Babysit()
-    print ('\nWelcome to the babysitter pay calculator! The babysitter gets paid ${0} \n'.format(babysitter.start_to_bed) +\
-           'for each hour between start time and bedtime, ${0} for each hour between \n'.format(babysitter.bed_to_mid) +\
-           'between bedtime and midnight, and ${0} for each hour after midnight. The \n'.format(babysitter.mid_to_end) +\
-           'babysitter cannot begin before 17:00 and must leave before 04:00. Partial \n' +\
-           'hours are rounded up to full hours. \n \n' +\
-           'Times must be entered in 24-hour format.\n' )
-        
+    print ('\nWelcome to the babysitter pay calculator! ' +
+           'The babysitter gets paid ${0} \nfor each hour between'
+           .format(babysitter.start_to_bed) +
+           ' start time and bedtime, ${0} '.format(babysitter.bed_to_mid) +
+           'for each hour between \nbetween bedtime and midnight' +
+           ',and ${0} for each hour after'.format(babysitter.mid_to_end) +
+           'midnight. The \n'
+           'babysitter cannot begin before 17:00 and must leave before ' +
+           '04:00. Partial \nhours are rounded up to full hours. \n\n' +
+           'Times must be entered in 24-hour format.\n')
+
     raw_start = raw_input('Please enter a starting time in HH:MM format.\n\n')
-                          
+
     starttime = babysitter.handle_start_input(raw_start)
     print('\n')
-    if starttime == None:
+    if starttime is None:
         sys.exit(0)
     raw_bed = raw_input('Please enter a bedtime in HH:MM format \n\n')
-                      
+
     bedtime = babysitter.handle_bed_input(raw_bed)
     print('\n')
-    if bedtime == None:
+    if bedtime is None:
         sys.exit(0)
     raw_end = raw_input('Please enter an ending time in HH:MM format \n\n')
-                       
+
     endtime = babysitter.handle_end_input(raw_end)
     print('\n')
-    if endtime == None:
+    if endtime is None:
         sys.exit(0)
     total_pay = babysitter.test_and_calculate(starttime, bedtime, endtime)
-    if total_pay == None:
-        print('Problem with times. Start and end times must be between \n' +\
-               '17:00 and 04:00. Bedtime must be before midnight. Endtime \n' + \
-              'be after bedtime, and bedtime must be after start time. '+ \
+    if total_pay is None:
+        print('Problem with times. Start and end times must be between \n' +
+              '17:00 and 04:00. Bedtime must be before midnight. Endtime \n' +
+              'be after bedtime, and bedtime must be after start time. ' +
               'Program now exiting.\n')
-    else: print ('The total pay will be ${0}\n'.format(total_pay))
+    else:
+        print ('The total pay will be ${0}\n'.format(total_pay))
     sys.exit(0)
--- babysitter_kata/test_babysitter.py
+++ babysitter_kata/test_babysitter.py
@@ -2,74 +2,103 @@ import unittest
 from datetime import time
 from babysitter import Babysit
 
+
 class testBabysitter(unittest.TestCase):
     """
     A test class for the Babysitter module
-    I assumed that the babysitter could work for 0 minutes (but get paid $0 for it),
+    I assumed that the babysitter could work for
+    0 minutes (but get paid $0 for it),
     and that the kids had to be in bed by midnight, which is reasonable.
     """
-    
+
     def setUp(self):
-        #init babysitter class
+        # init babysitter class
         self.babysitter = Babysit()
 
     def test_StartAndEndTime(self):
-        #can't start too early or end too late
-        self.assertTrue(Babysit.check_start_or_end_time(self.babysitter, time(17,00)))
-        self.assertTrue(Babysit.check_start_or_end_time(self.babysitter, time(04,00)) )      
-        self.assertFalse(Babysit.check_start_or_end_time(self.babysitter, time(04,01)))
-        self.assertFalse(Babysit.check_start_or_end_time(self.babysitter, time(16,59)) )       
+        # can't start too early or end too late
+        self.assertTrue(Babysit.check_start_or_end_time(
+            self.babysitter, time(17, 00)))
+        self.assertTrue(Babysit.check_start_or_end_time(
+            self.babysitter, time(04, 00)))
+        self.assertFalse(Babysit.check_start_or_end_time(
+            self.babysitter, time(04, 01)))
+        self.assertFalse(Babysit.check_start_or_end_time(
+            self.babysitter, time(16, 59)))
 
     def test_BedTime(self):
-        #kids need to be in bed before midnight, but after we've started sitting
-        self.assertTrue(Babysit.check_bed_time(self.babysitter, time(23,59)))
-        self.assertTrue(Babysit.check_bed_time(self.babysitter, time(17,00)))
-        self.assertFalse(Babysit.check_bed_time(self.babysitter, time(00,01)))
-        self.assertFalse(Babysit.check_bed_time(self.babysitter, time(04,59)))
-        
+        # kids need to be in bed before midnight,
+        # but after we've started sitting
+        self.assertTrue(Babysit.check_bed_time(self.babysitter, time(23, 59)))
+        self.assertTrue(Babysit.check_bed_time(self.babysitter, time(17, 00)))
+        self.assertFalse(Babysit.check_bed_time(self.babysitter, time(00, 01)))
+        self.assertFalse(Babysit.check_bed_time(self.babysitter, time(04, 59)))
+
     def test_HoursPaidFromStartToBed(self):
-        #make sure babysitter gets paid $12/hr from start to bedtime
-        self.assertEqual(36, Babysit.pay_start_to_bed(self.babysitter, time(17,00),time(20,00)))
-        self.assertEqual(12, Babysit.pay_start_to_bed(self.babysitter, time(17,00), time(17,01)))
-        self.assertEqual(12, Babysit.pay_start_to_bed(self.babysitter, time(23,00), time(00,00)))
+        # make sure babysitter gets paid $12/hr from start to bedtime
+        self.assertEqual(36, Babysit.pay_start_to_bed(
+            self.babysitter, time(17, 00), time(20, 00)))
+        self.assertEqual(12, Babysit.pay_start_to_bed(
+            self.babysitter, time(17, 00), time(17, 01)))
+        self.assertEqual(12, Babysit.pay_start_to_bed(
+            self.babysitter, time(23, 00), time(00, 00)))
 
     def test_HoursPaidFromBedToMid(self):
-        #get paid $8 between when the kids go to bed and midnight.
-        #partial hours are paid at higher $12 rate
-        #must calculate case where end time is before midnight
-        self.assertEqual(8, Babysit.pay_bed_to_mid(self.babysitter, time(22,30), time (01,00)))
-        self.assertEqual(16, Babysit.pay_bed_to_mid(self.babysitter, time(22,00),time(00,00)))
-        self.assertEqual(16, Babysit.pay_bed_to_mid(self.babysitter, time(17,30),time(20,00)))
-        self.assertEqual(24, Babysit.pay_bed_to_mid(self.babysitter, time(17,30),time(20,30)))
+        # get paid $8 between when the kids go to bed and midnight.
+        # partial hours are paid at higher $12 rate
+        # must calculate case where end time is before midnight
+        self.assertEqual(8, Babysit.pay_bed_to_mid(
+            self.babysitter, time(22, 30), time(01, 00)))
+        self.assertEqual(16, Babysit.pay_bed_to_mid(
+            self.babysitter, time(22, 00), time(00, 00)))
+        self.assertEqual(16, Babysit.pay_bed_to_mid(
+            self.babysitter, time(17, 30), time(20, 00)))
+        self.assertEqual(24, Babysit.pay_bed_to_mid(
+            self.babysitter, time(17, 30), time(20, 30)))
 
     def test_HoursPaidFromMidToEnd(self):
-        #if end time is after midnight, pay $16 an hour from midnight to end.
-        #partial hours count as full.
-        self.assertEqual(0, Babysit.pay_mid_to_end(self.babysitter, time(23,30)))
-        self.assertEqual(32, Babysit.pay_mid_to_end(self.babysitter, time(01,30)))
+        # if end time is after midnight, pay $16 an hour from midnight to end.
+        # partial hours count as full.
+        self.assertEqual(0, Babysit.pay_mid_to_end(self.babysitter,
+                                                   time(23, 30)))
+        self.assertEqual(32, Babysit.pay_mid_to_end(self.babysitter,
+                                                    time(01, 30)))
 
     def test_TotalPay(self):
-        #calculate total pay using previous methods
-        self.assertEqual(120, Babysit.calculate_total(self.babysitter, time(19,30), time(22,30), time(3,30)))
-        self.assertEqual(64, Babysit.calculate_total(self.babysitter, time(17,25), time(18,30), time(23,30)))
+        # calculate total pay using previous methods
+        self.assertEqual(120, Babysit.calculate_total(
+            self.babysitter, time(19, 30), time(22, 30), time(3, 30)))
+        self.assertEqual(64, Babysit.calculate_total(
+            self.babysitter, time(17, 25), time(18, 30), time(23, 30)))
 
     def test_IfTimesGivenAreRational(self):
-        #can't end before bedtime. bedtime can't be before start time.
-        self.assertTrue(Babysit.check_times_for_rationality(self.babysitter, time(18,30), time(18,45), time(19,00)))
-        self.assertTrue(Babysit.check_times_for_rationality(self.babysitter, time(18,00), time(19,30), time(19,45)))
-        self.assertFalse(Babysit.check_times_for_rationality(self.babysitter, time(18,45), time(18,30), time(19,00)))
-        self.assertFalse(Babysit.check_times_for_rationality(self.babysitter, time(18,00), time(19,45), time(19,30)))
-        self.assertFalse(Babysit.check_times_for_rationality(self.babysitter, time(19,30), time(19,45), time(19,00)))
+        # can't end before bedtime. bedtime can't be before start time.
+        self.assertTrue(Babysit.check_times_for_rationality(
+            self.babysitter, time(18, 30), time(18, 45), time(19, 00)))
+        self.assertTrue(Babysit.check_times_for_rationality(
+            self.babysitter, time(18, 00), time(19, 30), time(19, 45)))
+        self.assertFalse(Babysit.check_times_for_rationality(
+            self.babysitter, time(18, 45), time(18, 30), time(19, 00)))
+        self.assertFalse(Babysit.check_times_for_rationality(
+            self.babysitter, time(18, 00), time(19, 45), time(19, 30)))
+        self.assertFalse(Babysit.check_times_for_rationality(
+            self.babysitter, time(19, 30), time(19, 45), time(19, 00)))
 
     def test_EverythingInSequence(self):
-        #test if all methods run correctly when called by a single method        
-        self.assertEqual(120, Babysit.test_and_calculate(self.babysitter, time(19,30), time(22,30), time(3,30)))
-        self.assertEqual(None, Babysit.test_and_calculate(self.babysitter, time(13,30), time(22,30), time(3,30)))
-        self.assertEqual(None, Babysit.test_and_calculate(self.babysitter, time(20,30), time(19,30), time(3,30)))
+        # test if all methods run correctly when called by a single method
+        self.assertEqual(120, Babysit.test_and_calculate(
+            self.babysitter, time(19, 30), time(22, 30), time(3, 30)))
+        self.assertEqual(108, Babysit.test_and_calculate(
+            self.babysitter, time(17, 30), time(21, 30), time(1, 30)))
+        self.assertEqual(None, Babysit.test_and_calculate(
+            self.babysitter, time(13, 30), time(22, 30), time(3, 30)))
+        self.assertEqual(None, Babysit.test_and_calculate(
+            self.babysitter, time(20, 30), time(19, 30), time(3, 30)))
 
     def test_RawInputToTime(self):
-        #test if function can convert raw input to time object.        
-        self.assertEqual(time(12,34), self.babysitter.convert_raw_input('12:34'))
+        # test if function can convert raw input to time object.
+        self.assertEqual(time(12, 34),
+                         self.babysitter.convert_raw_input('12:34'))
         self.assertEqual(None, self.babysitter.convert_raw_input('25:34'))
         self.assertEqual(None, self.babysitter.convert_raw_input('12:65'))
         self.assertEqual(None, self.babysitter.convert_raw_input('123654'))
@@ -77,16 +106,19 @@ class testBabysitter(unittest.TestCase):
         self.assertEqual(None, self.babysitter.convert_raw_input('agwgawea'))
 
     def test_InputStartTime(self):
-        self.assertEqual(time(17,34), self.babysitter.handle_start_input('17:34'))        
+        self.assertEqual(time(17, 34),
+                         self.babysitter.handle_start_input('17:34'))
         self.assertEqual(None, self.babysitter.handle_start_input('asdfdfs'))
 
     def test_InputBedTime(self):
-        self.assertEqual(time(18,34), self.babysitter.handle_bed_input('18:34'))        
+        self.assertEqual(time(18, 34),
+                         self.babysitter.handle_bed_input('18:34'))
         self.assertEqual(None, self.babysitter.handle_bed_input('asdfdfs'))
 
     def test_InputEndTime(self):
-        self.assertEqual(time(18,34), self.babysitter.handle_end_input('18:34'))        
+        self.assertEqual(time(18, 34),
+                         self.babysitter.handle_end_input('18:34'))
         self.assertEqual(None, self.babysitter.handle_end_input('asdfdfs'))
 
 if __name__ == '__main__':
-    unittest.main()    
+    unittest.main()
